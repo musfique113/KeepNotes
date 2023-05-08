c57d7a809d6584699206e7733b99c5eda1c8b561
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-05-08 22:19:58 +0600

    all the listed requrements are add , have some bug , will fix in future

--- lib/home.dart
+++ lib/home.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     getAllNotes();
-    LocalDataSaver.saveSyncSet(false);
+    //LocalDataSaver.saveSyncSet(true);
   }
 
   Future createEntry(Note note) async {
